85d71839c22c253ee66249cec9989fad8784d18a
Emma Hou <emma.houy@gmail.com>
2016-11-27 15:11:45 -0500

    Add home page carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,19 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
+import { ProgramComponent } from './program/program.component';
 
 const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: '', component: HomeComponent }
+  { path: 'aboutus', component: AboutusComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'program', component: ProgramComponent },
+  { path: '', component: HomeComponent },
+  { path: '*', component: HomeComponent }
 ];
 
 @NgModule({
@@ -19,7 +28,11 @@ const appRoutes: Routes = [
     AppComponent,
     FooterComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    AboutusComponent,
+    ContactComponent,
+    LoginComponent,
+    ProgramComponent
   ],
   imports: [
     BrowserModule,
