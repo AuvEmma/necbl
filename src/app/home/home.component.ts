5f7ee2172351d417aa9f55b7f0304e8479742769
Zecheng Dong <Zecheng Dong>
2017-01-14 20:18:45 -0500

    Add HomePage and AboutUS

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+// import { HomeImage } from './home-image.interface';
+
 declare var $:any;
 
 @Component({
@@ -9,14 +11,19 @@ declare var $:any;
 })
 export class HomeComponent implements OnInit {
 
+  // public images = IMAGES;
+
   constructor() { }
 
-  ngOnInit() {
-    //  $('.slider').slider({
-    //    full_width: true,
-    //    indicators: false,
-    //    height: 600
-    //  });
-  }
+  ngOnInit() {}
 
 }
+
+// TODO
+// var IMAGES: HomeImage[] = [
+//   { "title": "We are covered", "url": "images/covered.jpg" },
+//   { "title": "Generation Gap", "url": "images/generation.jpg" },
+//   { "title": "Potter Me", "url": "images/potter.jpg" },
+//   { "title": "Pre-School Kids", "url": "images/preschool.jpg" },
+//   { "title": "Young Peter Cech", "url": "images/soccer.jpg" } 
+// ];
