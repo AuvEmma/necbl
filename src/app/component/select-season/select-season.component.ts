0d81a1813db29ff68973a43fcd312fc1a8a5eb29
Emma Hou <emma.houy@gmail.com>
2017-02-03 15:47:56 -0500

    User able to select region at application

--- src/app/component/select-season/select-season.component.ts
+++ src/app/component/select-season/select-season.component.ts
@@ -3,6 +3,8 @@ import { Router }                   from '@angular/router';
 import { ApplicationService }       from '../../services'
 import { LoginService }             from '../../services'
 
+declare var $:any;
+
 @Component({
   selector: 'app-select-season',
   templateUrl: './select-season.component.html',
@@ -15,6 +17,7 @@ export class SelectSeasonComponent implements OnInit {
   selectOptions : any     = [];
   isAdmin       : boolean = true;
   subs          : any     = [];
+  regions       : any     = [];
 
   constructor(private _loginService: LoginService, private _applicationService: ApplicationService) {
     let sub: any = this._loginService.isAdmin$.subscribe(isAdmin => this.isAdmin = isAdmin, error => {/*console.log('Error: ', error)*/});
@@ -23,15 +26,26 @@ export class SelectSeasonComponent implements OnInit {
 
   ngOnInit() {
     localStorage.removeItem('season');
+    localStorage.removeItem('region');
+
     this._applicationService.getSeasons()
       .subscribe(
         data  => {
-          if (Array.isArray(data)) this.seasons = data;
+          if (Array.isArray(data)) this.seasons = data; this.regions = data.regions;
         },error => console.error('error',error)
       )
       window.setTimeout(()=>{
         this.selectOptions = []
       },50);
+  }
+
+  getRegions(){
+    for (let i = 0; i < this.seasons.length; i++) {
+        if (this.seasons[i].name === this.seasonName) {
+            this.regions = this.seasons[i].regions
+        };
+    }
+    $('select').material_select();
   }
 
 }
