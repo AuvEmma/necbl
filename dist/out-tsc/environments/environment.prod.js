c81b87afe65eb8c8c80b58626a2d5054502e9241
Emma Hou <emma.houy@gmail.com>
2017-02-03 18:44:55 -0500

    Build

--- dist/out-tsc/environments/environment.prod.js
+++
@@ -1,7 +0,0 @@
-export var environment = {
-    production: true,
-    serverProtocol: 'http://',
-    serverUrl: '54.237.242.64',
-    serverPort: '3001'
-};
-//# sourceMappingURL=../../../src/environments/environment.prod.js.map
