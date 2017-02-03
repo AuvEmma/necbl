30e57a5976eff311a835c9ff7480052ca8efb6bf
Emma Hou <emma.houy@gmail.com>
2017-02-03 18:44:10 -0500

    Change env

---
+++ dist/out-tsc/environments/environment.prod.js
@@ -0,0 +1,7 @@
+export var environment = {
+    production: true,
+    serverProtocol: 'http://',
+    serverUrl: '54.237.242.64',
+    serverPort: '3001'
+};
+//# sourceMappingURL=../../../src/environments/environment.prod.js.map
