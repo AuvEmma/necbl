b92e6334d35929b53e1723590cc50bf0ae698099
Emma Hou <emma.houy@gmail.com>
2017-02-09 21:30:15 -0500

    new build

--- dist/styles.58e065928ed8ebd0b582.bundle.js
+++ dist/styles.58e065928ed8ebd0b582.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([2,3],{1002:function(n,o,c){c(417),n.exports=c(416)},416:function(n,o){},417:function(n,o){}},[1002]);
+webpackJsonp([2,3],{1046:function(n,o,c){c(422),n.exports=c(421)},421:function(n,o){},422:function(n,o){}},[1046]);
 //# sourceMappingURL=styles.58e065928ed8ebd0b582.bundle.map
